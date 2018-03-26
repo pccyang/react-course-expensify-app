36952544fca50c0e87d08325d2801b093a485ca1
CCYang <ccyang@CCYangs-MacBook-Pro.local>
2018-03-26 15:04:10 -0700

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,62 @@
 
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin'); ''
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
-}
+module.exports = (env) => {
+  const isProductrion = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+            // use: [
+            //   'css-loader',
+            //   'sass-loader'
+            // ]
+          })
+          // use: [
+          //   'style-loader',
+          //   'css-loader',
+          //   'sass-loader'
+          // ]
+        }
+      ]
+    },
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProductrion ? 'source-map' : 'inline-source-map',
+    // devtool: isProductrion ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  };
+};
